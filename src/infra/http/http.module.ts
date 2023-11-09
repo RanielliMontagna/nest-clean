928ff5345a42438ca460aceed3798662885ccdfa
Ranielli Montagna <raniellimontagna@hotmail.com>
2023-11-09 19:48:17 -0300

    feat: :sparkles: cria listagem de perguntas recentes

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -5,6 +5,8 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 import { DatabaseModule } from '../database/database.module'
+import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 
 @Module({
   imports: [DatabaseModule],
@@ -14,5 +16,6 @@ import { DatabaseModule } from '../database/database.module'
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
+  providers: [CreateQuestionUseCase, FetchRecentQuestionsUseCase],
 })
 export class HttpModule {}
